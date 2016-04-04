8e9446e91c7a9992d514354f581de1511be4fe3c
Uchio KONDO <udzura@pepabo.com>
2016-04-04 18:29:06 +0900

    The check runner

--- cmd/client.go
+++ cmd/client.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	// "fmt"
+	"errors"
+	"os"
+	"os/exec"
+	"syscall"
+
 	"github.com/spf13/cobra"
 )
 
@@ -13,11 +19,38 @@ var clientCmd = &cobra.Command{
 	Long: `collector's client command.
 This subcommand ins intendec to run under consul checker.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		logger.Infof("client called: %v", args)
+		ret := runCheck(args)
+		os.Exit(ret)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(clientCmd)
 	clientCmd.Flags().StringVarP(&deviceName, "dev", "D", "eth0", "Device name which has the instance's global IP")
+}
+
+func runCheck(args []string) int {
+	var exitStatus int
+	if len(args) < 1 {
+		logger.Errorln("Check command is empty.")
+		return -1
+	}
+
+	check := exec.Command(args[0], args[1:]...)
+	_, err := check.Output()
+	if err != nil {
+		if e2, ok := err.(*exec.ExitError); ok {
+			if s, ok := e2.Sys().(syscall.WaitStatus); ok {
+				exitStatus = s.ExitStatus()
+			} else {
+				panic(errors.New("Unimplemented for system where exec.ExitError.Sys() is not syscall.WaitStatus."))
+			}
+		}
+	} else {
+		exitStatus = 0
+	}
+
+	logger.Infof("client called: %v", args)
+	logger.Infof("command exit with %d", exitStatus)
+	return exitStatus
 }
